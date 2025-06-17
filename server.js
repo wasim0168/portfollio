91e372e4b7f7a7226bf18c474eda288aecddfc75
𝕸𝖔𝖍𝖘𝖎𝖓 𝕾𝖍𝖊𝖎𝖐𝖍 . <wasimsheikh0168@gmail.com>
2025-06-17 13:56:52 +0530

    index

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/' , (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'main.html')); 
+    res.sendFile(path.join(__dirname, 'public' ,  'index.html')); 
     });
 
 
